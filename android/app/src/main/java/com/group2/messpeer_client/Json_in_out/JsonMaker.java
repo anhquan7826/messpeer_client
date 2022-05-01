6e56d24503901d13a4b6afcfc666f58895dc2c11
coder-newbie404 <74915494+coder-newbie404@users.noreply.github.com>
2022-05-01 14:33:54 +0700

    eos hieeur looxi gif nhuwng du method rooi

--- android/app/src/main/java/com/group2/messpeer_client/Json_in_out/JsonMaker.java
+++ android/app/src/main/java/com/group2/messpeer_client/Json_in_out/JsonMaker.java
@@ -1,17 +1,13 @@
 package com.group2.messpeer_client.Json_in_out;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
 
 public class JsonMaker {
 
+    /*
     public static HashMap<String, String> ReadJson(String path) {
         HashMap<String, String> output = new HashMap<>();
 
@@ -28,27 +24,57 @@ public class JsonMaker {
             e.printStackTrace();
         }
         return output;
-    }
+    }*/
 
-    public static void MakeJson(String groupID, String userID, String message, String time) {
+    public static String MessagesJson(String userID, String groupID, String message, String time) {
         JSONObject jsonObject = new JSONObject();
         jsonObject.put("groupID", groupID);         //tao noi dung cho json
         jsonObject.put("UserID", userID);           //tao noi dung cho json
         jsonObject.put("Message", message);         //tao noi dung cho json
         jsonObject.put("Time", time);               //tao noi dung cho json
         try {
-            FileWriter file = new FileWriter("src/main/java/output.json"); //sua thu muc ghi ra o cho nay
+            FileWriter file = new FileWriter("src/main/java/messages.json"); //sua thu muc ghi ra o cho nay
             file.write(jsonObject.toJSONString());
             file.close();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        System.out.println("JSON file created: "+jsonObject);
+        return jsonObject.toJSONString();
+    }
+
+    public static String GroupJson(String groupID) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("groupID", groupID);         //tao noi dung cho json
+        try {
+            FileWriter file = new FileWriter("src/main/java/group.json");  //sua thu muc ghi ra o cho nay
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return jsonObject.toJSONString();
+    }
+
+    public static String UserJson(String userID) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("userID", userID);         //tao noi dung cho json
+        try {
+            FileWriter file = new FileWriter("src/main/java/user.json");  //sua thu muc ghi ra o cho nay
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return jsonObject.toJSONString();
     }
 
     public static void main(String[] args) {
-        MakeJson("123456", "1357", "co cai ccc", "22-11-2022");
+        MessagesJson("123456", "1357", "co cai ccc", "22-11-2022");
+        GroupJson("123456");
+        UserJson("1357");
     }
 }
 
