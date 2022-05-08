16076298ba4c0609591fd540c1a88b5f7f2804ed
anhquan7826 <anhquan7826@gmail.com>
2022-05-08 23:59:12 +0700

    login route finished?

--- lib/login_screen/login.dart
+++ lib/login_screen/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:messpeer_client/utils/BackendMethod.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -26,69 +26,176 @@ class _LoginState extends State<Login> {
     _passwordController.dispose();
   }
 
+  bool _isLoading = false;
+  bool _loginResult = false;
+
+  Future<void> _login(String username, String password) async {
+    setState(() {
+      _isLoading = true;
+    });
+    _loginResult = await BackendMethod.getInstance()!.authenticate(username, password);
+    setState(() {
+      _isLoading = false;
+      if (_loginResult) {
+        Navigator.pushReplacementNamed(context, '/overview', arguments: {
+          // TODO: get userdata
+        });
+      } else {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              backgroundColor: Colors.blueGrey[900],
+              title: const Text(
+                'Error',
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+              content: const Text(
+                'Incorrect username or password!',
+                style: TextStyle(
+                    color: Colors.white
+                ),
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                )
+              ],
+            );
+          },
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const SizedBox(
-            height: 100.0,
-          ),
-          const CircleAvatar(
-            radius: 100.0,
-            child: Icon(
-              Icons.person,
-              size: 75.0,
+          const Text(
+            'Messpeer',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40.0,
+              fontWeight: FontWeight.bold,
             ),
           ),
           const SizedBox(
-            height: 50.0,
+            height: 40.0,
+          ),
+          const Text(
+            'Please enter your credential...',
+            style: TextStyle(
+                color: Colors.white60,
+                fontSize: 15.0
+            ),
+          ),
+          const SizedBox(
+            height: 30.0,
           ),
           Padding(
             padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+            const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
             child: TextField(
               autocorrect: false,
               enableSuggestions: false,
               controller: _usernameController,
+              style: const TextStyle(
+                  color: Colors.white
+              ),
               decoration: InputDecoration(
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50),
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 hintText: "Username...",
+                hintStyle: const TextStyle(
+                  color: Colors.white30,
+                ),
+                filled: true,
+                fillColor: Colors.blueGrey[700],
               ),
               onChanged: (value) {},
             ),
           ),
           Padding(
             padding:
-                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+            const EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
             child: TextField(
               autocorrect: false,
               enableSuggestions: false,
               controller: _passwordController,
               obscureText: true,
+              style: const TextStyle(
+                  color: Colors.white
+              ),
               decoration: InputDecoration(
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50),
+                  borderRadius: BorderRadius.circular(10),
                 ),
                 hintText: "Password...",
+                hintStyle: const TextStyle(
+                  color: Colors.white30,
+                ),
+                filled: true,
+                fillColor: Colors.blueGrey[700],
               ),
               onChanged: (value) {},
             ),
           ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Sign in'),
+          const SizedBox(height: 40.0,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0),
+            child: ElevatedButton(
+              onPressed: () {
+                String username = _usernameController.text;
+                String password = _passwordController.text;
+                _login(username, password);
+              },
+              child: _isLoading ? const CircularProgressIndicator(
+                color: Colors.white,
+              ) : const Text('Sign in'),
+              style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  minimumSize: const Size(400.0, 50.0)
               ),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Sign up'),
-              )
-            ],
+            ),
+          ),
+          const Divider(
+            indent: 40.0,
+            endIndent: 40.0,
+            height: 40.0,
+          ),
+          const Text(
+            "Don't have an account?",
+            style: TextStyle(
+              color: Colors.white60,
+            ),
+          ),
+          const SizedBox(height: 20.0,),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50.0),
+            child: ElevatedButton(
+              onPressed: () {  },
+              child: const Text('Sign up'),
+              style: ElevatedButton.styleFrom(
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  minimumSize: const Size(400.0, 50.0)
+              ),
+            ),
           ),
         ],
       ),
