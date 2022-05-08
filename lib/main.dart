db1e59c7e3b64211241e83b7f33969d1e8835799
anhquan7826 <anhquan7826@gmail.com>
2022-05-08 23:59:19 +0700

    idk

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:messpeer_client/chat_screen/chat.dart';
 import 'package:messpeer_client/login_screen/login.dart';
 import 'package:messpeer_client/overview_screen/overview.dart';
-import 'loading_screen/loading.dart';
 
 void main() {
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
@@ -18,14 +18,12 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blueGrey,
         ),
-        home: Overview()
-        // initialRoute: '/chat',
-        // routes: {
-        //   '/': (context) => Loading(),
-        //   '/login': (context) => Login(),
-        //   '/overview': (context) => Overview(),
-        //   '/chat': (context) => Chat(),
-        // },
+        initialRoute: '/login',
+        routes: {
+          '/login': (context) => const Login(),
+          '/overview': (context) => const Overview(),
+          '/chat': (context) => const Chat(),
+        },
         );
   }
 }
