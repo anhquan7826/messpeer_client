d1661ab4493c95bea8f943b8b0c6df1a5d56e44c
anhquan7826 <anhquan7826@gmail.com>
2022-05-08 23:59:36 +0700

    method channel

--- android/app/src/main/java/com/group2/messpeer_client/MainActivity.java
+++ android/app/src/main/java/com/group2/messpeer_client/MainActivity.java
@@ -1,66 +1,71 @@
 package com.group2.messpeer_client;
 
+import android.os.Build;
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-
-import com.group2.messpeer_client.Json_in_out.JsonMaker;
+import androidx.annotation.RequiresApi;
 import com.group2.messpeer_client.connection.Connection;
-import com.group2.messpeer_client.connection.ServerCommunicator;
-
+import java.util.Objects;
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
-    Connection connection = new Connection();
-    ServerCommunicator serverCommunicator;
+    Connection connection;
 
     public static final String CHANNEL = "com.group2.messpeer_client/communication";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        connection.start();
-        try {
-            serverCommunicator = connection.getServerCommunicator();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        connection = new Connection();
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
 
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
                 (call, result) -> {
-                    if (call.method.equals("authenticate")) {
-                        String username = call.argument("username");
-                        String password = call.argument("password");
-                        result.success(connection.authenticate(username, password));
-                    } else if (call.method.equals("sendMessage")) {
-                        String username = call.argument("username");
-                        String groupChatID = call.argument("groupChatID");
-                        String message = call.argument("messageObject");
-                        // TODO: turn raw message to json object
-                        serverCommunicator.getCommandQueue().add(JsonMaker.MessagesJson(username, groupChatID, message, "22-11-22"));
-                    } else if (call.method.equals("groupChatCreate")) {
-                        String groupChatID = call.argument("groupChatID");
-                        serverCommunicator.getCommandQueue().add(JsonMaker.GroupJson(groupChatID));
-                    } else if (call.method.equals("groupChatDelete")) {
-                        String groupChatID = call.argument("groupChatID");
-                        serverCommunicator.getCommandQueue().add(JsonMaker.GroupJson(groupChatID));
-                    } else if (call.method.equals("groupChatAdd")) {
-                        String username = call.argument("username");
-                        serverCommunicator.getCommandQueue().add(JsonMaker.UserJson(username));
-                    } else if (call.method.equals("groupChatKick")) {
-                        String username = call.argument("username");
-                        serverCommunicator.getCommandQueue().add(JsonMaker.UserJson(username));
-                    } else if (call.method.equals("groupChatChangeHost")) {
-                        String username = call.argument("username");
-                        serverCommunicator.getCommandQueue().add(JsonMaker.UserJson(username));
+                    String username = call.argument("username");
+                    String groupChatName = call.argument("groupChatName");
+                    String groupChatID = call.argument("groupChatID");
+                    String message = call.argument("messageObject");
+                    String password = call.argument("password");
+                    String command = "";
+                    switch (call.method) {
+                        case "authenticate":
+                            result.success(connection.authenticate(username, password));
+                            break;
+                        case "getMessage":
+                            result.success(connection.pollMessages());
+                            break;
+                        case "sendMessage":
+                            // TODO: fix this
+                            //connection.sendMessage(JsonMaker.MessagesJson(username, groupChatID, message, getCurrentDateTime()));
+                            break;
+/*                        case "groupChatCreate":
+                            command = "GROUP_CHAT_CREATE:" + groupChatName;
+                            result.success(querryCommand(command));
+                            break;
+                        case "groupChatDelete":
+                            command = "GROUP_CHAT_DELETE:" + groupChatID;
+                            result.success(querryCommand(command));
+                            break;
+                        case "groupChatAdd":
+                            command = "GROUP_CHAT_ADD:" + username + " " + groupChatID;
+                            result.success(querryCommand(command));
+                            break;
+                        case "groupChatKick":
+                            command = "GROUP_CHAT_KICK:" + username + " " + groupChatID;
+                            result.success(querryCommand(command));
+                            break;
+                        case "groupChatChangeHost":
+                            command = "GROUP_CHAT_CHANGE_HOST:" + username + " " + groupChatID;
+                            result.success(querryCommand(command));
+                            break;*/
                     }
                 }
         );
