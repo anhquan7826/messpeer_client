4b068d64c75da9d776b82842903806cc2fb78ad3
anhquan7826 <anhquan7826@gmail.com>
2022-05-12 20:27:41 +0700

    call state

---
+++ lib/utils/CallState.dart
@@ -0,0 +1,7 @@
+class CallState {
+  static const String CALLING = "CALLING";
+  static const String IDLE = "IDLE";
+  static const String TRUE = "TRUE";
+  static const String FALSE = "FALSE";
+  static const String ERROR = "ERROR";
+}
